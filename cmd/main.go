37a0d1fd18922c7e8a91b01a3c19e39bcb9239ae
Nils Carstensen <me@nils-carstensen.de>
2022-04-13 16:04:35 +0200

    Refactored: Holgerdocs functionallity.

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
-	"os"
 	"path/filepath"
-	"text/template"
 
 	"github.com/alexflint/go-arg"
 )
@@ -25,36 +22,6 @@ func main() {
 	}
 
 	if args.ActionGroup == "holgerdocs" {
-		temp := (createDocs(absolutePath))
-
-		// Collect existing README content
-		existingContent := parseMarkdown(absolutePath + "/README.md")
-		markdownContent := MarkdownContent{Title: existingContent["title"], Description: existingContent["description"], ExampleUsage: existingContent["example_usage"], Variables: temp["variables"], Outputs: temp["outputs"]}
-
-		// Template rendering
-		templateFilePath, err := filepath.Abs("templates/holgerdocs.tmpl")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		templateContent, err := ioutil.ReadFile(templateFilePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		tmpl, err := template.New("holgerdocs").Parse(string(templateContent))
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		f, err := os.Create(absolutePath + "/temp.md")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = tmpl.Execute(f, markdownContent)
-		if err != nil {
-			log.Fatal(err)
-		}
+		holgerdocs(absolutePath, "terraform")
 	}
 }
