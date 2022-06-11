0d3507ee737b48814d062162da0f843e28167894
Nils Carstensen <me@nils-carstensen.de>
2022-06-11 23:34:16 +0200

    Added: Helpers package

---
+++ pkg/helpers/helpers.go
@@ -0,0 +1,34 @@
+package helpers
+
+import (
+	"crypto/sha256"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+)
+
+func getAbsPathAndReadFile(path string) string {
+
+	// Create absolute path to config file
+	absFilePath, err := filepath.Abs(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read Config File
+	fileContent, err := ioutil.ReadFile(absFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(fileContent)
+
+}
+
+func calcFileChecksum(path string) [32]byte {
+
+	sum := sha256.Sum256([]byte(getAbsPathAndReadFile(path)))
+
+	return sum
+
+}
